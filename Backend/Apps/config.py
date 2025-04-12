bef830a2805bbe6089c75f04561d72b693de5139
QuniteZone <1946614137@qq.com>
2025-04-12 12:32:34 +0800

    备注备注

--- Backend/Apps/config.py
+++ Backend/Apps/config.py
@@ -1,2 +1,11 @@
+import json
+import os
+import openai
+
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/test'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+#LLM 基本配置信息
+os.environ["OPENAI_BASE_URL"] = "https://api.chatanywhere.tech/v1"
+os.environ["OPENAI_API_KEY"] = "sk-OyEPaIflRbJXIospoq197kPskfatY1UmbfKKOszLJicK7RuJ"
+model = "gpt-4o-mini"
